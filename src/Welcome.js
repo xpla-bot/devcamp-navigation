01e8f7d916010488b1d8d060eb33626531fdc8b2
grin <grinkiewicz@gmail.com>
2017-10-12 15:38:48 +0200

    Pozwól zamknąć ekran modalny

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,6 +3,19 @@ import { View, Text } from 'react-native';
 import styles from './styles';
 
 export default class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+  onNavigatorEvent = event => {
+    if (event.type === 'NavBarButtonPress') {
+      if (event.id === 'close') {
+        this.props.navigator.dismissModal({ animationType: 'slide-down' });
+      }
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>

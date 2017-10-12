de84da875634623bb3dc59a5a7ee27e38e3c6c40
grin <grinkiewicz@gmail.com>
2017-10-12 15:38:35 +0200

    Pokaż powitanie na ekranie modalnym

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import Welcome from './Welcome';
 import HomeIcon from './icons/home.png';
 import SettingsIcon from './icons/settings.png';
 import HomeTab from './HomeTab';
+import SettingsTab from './SettingsTab';
 
 export default function startApp() {
   Navigation.registerComponent('screen.welcome', () => Welcome);
   Navigation.registerComponent('screen.homeTab', () => HomeTab);
+  Navigation.registerComponent('screen.settingsTab', () => SettingsTab);
 
   Navigation.startTabBasedApp({
     tabs: [
@@ -20,7 +22,7 @@ export default function startApp() {
         label: 'Settings',
         icon: SettingsIcon,
         title: 'Settings',
-        screen: 'screen.welcome'
+        screen: 'screen.settingsTab'
       }
     ],
     animationType: 'fade'

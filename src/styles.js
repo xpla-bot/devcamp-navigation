dda9f40f2c880836de1701083a78a0f44415e893
grin <grinkiewicz@gmail.com>
2017-10-12 15:24:31 +0200

    Dodaj ikonki i style

---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const blueColor = '#0A7FFC';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
